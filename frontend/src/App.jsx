011a37d3e8500a9518e273cf074f14913d58df46
letzkhelo <letzkhello@gmail.com>
2025-01-09 05:27:06 +0530

    pushed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+
+
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
@@ -6,22 +8,27 @@ import PageNotFound from "./components/PageNotFound";
 import { Toaster } from 'react-hot-toast';
 import { useEffect, useState } from "react";
 
-
 const App = () => {
-
-  const [token, setToken] = useState(localStorage.getItem("jwt"));
+  const [token, setToken] = useState(null);
+  const [isTokenLoaded, setIsTokenLoaded] = useState(false);
 
   useEffect(() => {
     const storedToken = localStorage.getItem("jwt");
     setToken(storedToken);
+    setIsTokenLoaded(true); // Ensure we wait for token initialization
   }, []);
+
+  if (!isTokenLoaded) {
+    // Display a loading screen while the token is being checked
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <Routes >
-
+      <Routes>
         <Route
           path="/"
-          element={token ? <Home /> : <Navigate to={"/login"} />}
+          element={token ? <Home /> : <Navigate to="/login" />}
         />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
@@ -29,7 +36,7 @@ const App = () => {
       </Routes>
       <Toaster />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
