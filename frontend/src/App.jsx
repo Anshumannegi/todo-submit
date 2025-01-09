23c1215d10f8050c33bfcb9d96385630f8a7bd2c
letzkhelo <letzkhello@gmail.com>
2025-01-09 05:46:39 +0530

    Pushed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,23 @@
 
 
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import PageNotFound from "./components/PageNotFound";
 import { Toaster } from 'react-hot-toast';
-import { useEffect, useState } from "react";
+
 
 const App = () => {
-  const [token, setToken] = useState(null);
-  const [isTokenLoaded, setIsTokenLoaded] = useState(false);
 
-  useEffect(() => {
-    const storedToken = localStorage.getItem("jwt");
-    setToken(storedToken);
-    setIsTokenLoaded(true); // Ensure we wait for token initialization
-  }, []);
 
-  if (!isTokenLoaded) {
-    // Display a loading screen while the token is being checked
-    return <div>Loading...</div>;
-  }
 
   return (
     <div>
       <Routes>
         <Route
           path="/"
-          element={token ? <Home /> : <Navigate to="/login" />}
-        />
+          element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/*" element={<PageNotFound />} />
